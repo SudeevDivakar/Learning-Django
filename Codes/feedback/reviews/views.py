d9bc6e0e2753067a5a9aa376cb202bcbc3ccf878
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-07-15 19:04:06 +0530

    Finish Class Views

--- Codes/feedback/reviews/views.py
+++ Codes/feedback/reviews/views.py
@@ -1,30 +1,55 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+'''Class Based View Implementation using Create View'''
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+
+'''Class Based View Implementation using Form View'''
+# class ReviewView(FormView):
+#     form_class = ReviewForm
+#     template_name = "reviews/review.html"
+#     success_url = "/thank-you"
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
+
+
+'''Class Based View Implementation using Base View'''
+# class ReviewView(View):
+#     def get(self, request):
+#         form = ReviewForm()
         
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect("/thank-you")
         
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+#         return render(request, "reviews/review.html", {
+#             "form": form
+#         })
 
 
+'''Function implementation'''
 # def review(request):
 #     if request.method == 'POST':
 #         form = ReviewForm(request.POST)
@@ -41,5 +66,51 @@ class ReviewView(View):
 #         "form": form
 #     })
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+'''Class Based View Implementation using Template View'''
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+'''Function implementation'''
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
+
+'''Class Based View Implementation using List View'''
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+'''Class Based View Implementation using Template View'''
+# class ReviewsListView(TemplateView):
+#     template_name = "reviews/review_list.html"
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         reviews = Review.objects.all()
+#         context["reviews"] = reviews
+#         return context
+   
+'''Class Based View Implementation using Detail View'''
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
+
+'''Class Based View Implementation using Template View'''
+# class SingleReviewView(TemplateView):
+#     template_name = "reviews/single_review.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         review_id = kwargs["id"]
+#         selected_review = Review.objects.get(pk=review_id)
+#         context["review"] = selected_review
+#         return context
